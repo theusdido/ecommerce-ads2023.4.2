841c13d9ec14bcd1b2d6434609a83e2f9f6cf4df
EDILSON VALENTIM DOS SANTOS BITENCOURT <edilson.bitencourt@prof.sc.senac.br>
2023-08-22 19:29:13 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:HomeComponent},
+  { path:'home', component:HomeComponent},
+  { 
+    path:'categoria', 
+    component:CategoriaComponent,
+    children:[
+      {path:'' , component:CategoriaListarComponent},
+      {path:'listar', component:CategoriaListarComponent},
+      {path:'form', component:CategoriaFormComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MenuComponent,
+    HomeComponent,
+    CategoriaComponent,
+    CategoriaListarComponent,
+    CategoriaFormComponent
   ],
   imports: [
     BrowserModule,
